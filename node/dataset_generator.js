a3d612bac459a58108f1601b4dae792dfb87b684
gramjos <graham.joss@gmail.com>
2023-06-24 17:01:53 -0500

    progress indicator

--- node/dataset_generator.js
+++ node/dataset_generator.js
@@ -1,17 +1,11 @@
 const draw=require('../common/draw'); 
+const constants=require('../common/constants.js'); 
+const utils=require('../common/utils.js'); 
 
 const {createCanvas}=require('canvas');
 const canvas=createCanvas(400,400);
 const ctx=canvas.getContext('2d');
 
-const constants={}; 
-
-constants.DATA_DIR="/Users/g_joss/Documents/Computation/projects/drawing_classifier/data"; 
-constants.RAW_DIR=constants.DATA_DIR+"/raw/"; 
-constants.DATASET_DIR=constants.DATA_DIR+"/dataset"; 
-constants.JSON_DIR=constants.DATASET_DIR+"/json";
-constants.IMG_DIR=constants.DATASET_DIR+"/img";
-constants.SAMPLES=constants.DATASET_DIR+"/samples.json";
 
 //use the file system
 const fs=require('fs');
@@ -42,6 +36,8 @@ fileNames.forEach(fn=>{
         const paths= drawings[label]; 
         fs.writeFileSync(constants.JSON_DIR+"/"+id+".json", JSON.stringify(paths)); 
         generateImageFile(constants.IMG_DIR+"/"+id+".png",paths); 
+        // time 8 b/c every file has 8 drawings
+        utils.printProgress(id,fileNames.length*8); 
         id++; 
     }
 }); 
